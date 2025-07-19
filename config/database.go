a9869b2c2ef6b441f66a136ddcb463061988f81f
= <=>
2025-07-19 12:39:09 +0000

    fix: database laoding started

--- config/database.go
+++ config/database.go
@@ -15,16 +15,10 @@ var DB *gorm.DB
 func ConnectDB() {
 	godotenv.Load()
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
-
-	// --- THE FIX IS HERE ---
-	// Declare the 'err' variable before using it for assignment.
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("FATAL: DATABASE_URL environment variable not set")
+	}
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
